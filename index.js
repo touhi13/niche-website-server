ae473a667cdb89edb8cdbfe98371af5a2fa36fa2
touhi13 <touhi13@gmail.com>
2021-11-13 18:32:16 +0600

    make it better

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Doctors portal!')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+})
