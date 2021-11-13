ae819e9041d4c1656087f8a0ec90c2bf640e8733
touhi13 <touhi13@gmail.com>
2021-11-13 18:54:49 +0600

    make it better

--- index.js
+++ index.js
@@ -16,6 +16,26 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
+console.log(process.env.DB_PASS)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xnb9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('Hello Doctors portal!')
 })
